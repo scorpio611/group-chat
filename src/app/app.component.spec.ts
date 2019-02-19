6328cec87cf70b6df755ef11e61d29eb58010e7b
scorpio611 <evilmonter666@gmail.com>
2019-02-19 21:47:47 +0700

    Case Study

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'typescript-chat-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('typescript-chat-client');
+    expect(app.title).toEqual('client');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to typescript-chat-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to client!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'tcc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'typescript-chat-client';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  private initModel(): void {
+  }
 }
